028ccde589d9c9c4269f8bb4367829a9b5921ac4
woalshue <sonjaemin33@gmail.com>
2026-07-20 13:17:12 +0900

    feat: NodeFactory, NodeDescriptor, NodeRegistry 및 관련 예외 클래스 구현

---
+++ src/main/java/site/omagotchi/ruleservice/core/registry/exception/NodeRegistryException.java
@@ -0,0 +1,10 @@
+package site.omagotchi.ruleservice.core.registry.exception;
+
+/**
+ * 부모
+ */
+public class NodeRegistryException extends RuntimeException {
+    public NodeRegistryException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/site/omagotchi/ruleservice/core/registry/exception/UnknownNodeTypeException.java
@@ -0,0 +1,7 @@
+package site.omagotchi.ruleservice.core.registry.exception;
+
+public class UnknownNodeTypeException extends NodeRegistryException {
+    public UnknownNodeTypeException(String message) {
+        super(message);
+    }
+}
