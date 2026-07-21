93f1ff0a96dfc6b1156d19c4ad4daa0392a63659
woalshue <sonjaemin33@gmail.com>
2026-07-21 20:42:05 +0900

    refactor: FlowEntry Record로 변경 및 테스트 코드에서 프로필 설정

--- src/main/java/site/omagotchi/ruleservice/core/engine/FlowEntry.java
+++ src/main/java/site/omagotchi/ruleservice/core/engine/FlowEntry.java
@@ -1,16 +1,12 @@
 package site.omagotchi.ruleservice.core.engine;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import site.omagotchi.ruleservice.core.parser.definition.FlowDefinition;
 
 /**
  * FlowManager가 "이 플로우가 어떤 정의로 배포됐는지"를 기억해두는 용도
  * 원본 정의(JSON에서 온 것)를 보관
  */
-@RequiredArgsConstructor
-@Getter
-class FlowEntry {
-
-    private final FlowDefinition flowDefinition;
+record FlowEntry(
+        FlowDefinition flowDefinition
+) {
 }
