680935b4491addad51bd0f99f0205a871320a637
woalshue <sonjaemin33@gmail.com>
2026-07-28 17:49:44 +0900

    fix: REST API 경로 변경 (/api/v{n}/**)

--- src/main/java/site/omagotchi/ruleservice/flow/presentation/FlowController.java
+++ src/main/java/site/omagotchi/ruleservice/flow/presentation/FlowController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/flows")
+@RequestMapping("/api/v1/flows")
 @RequiredArgsConstructor
 public class FlowController {
 
