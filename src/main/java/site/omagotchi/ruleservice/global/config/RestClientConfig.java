a867e4138d701b451ead2fbf53e2d0b2423a2119
woalshue <sonjaemin33@gmail.com>
2026-08-11 19:31:18 +0900

    feat: engineInternalRestClient가 공유 시크릿 헤더를 자동으로 실어 보내도록 변경

--- src/main/java/site/omagotchi/ruleservice/global/config/RestClientConfig.java
+++ src/main/java/site/omagotchi/ruleservice/global/config/RestClientConfig.java
@@ -24,19 +24,16 @@ public class RestClientConfig {
     /**
      * 엔진 간 REST 폴링 전용 RestClient
      * baseUrl을 고정하지 않음 - 매번 다른 피어(engine-a, engine-b, ...)의 주소로 호출해야 하고, 그 주소는 Eureka에서 그때그때 조회해서 얻기 때문
-     * connect/read 타임아웃을 각각 2초로 명시 - 이 값은 learning-service 호출과 무관하게, 엔진 생존 판정 폴링 주기(3초)보다 짧아야 한다는 distributed feature 자체의 요구사항에서 나온
      */
     @Bean
-    public RestClient engineInternalRestClient() {
+    public RestClient engineInternalRestClient(@Value("${internal.shared-secret}") String sharedSecret) {
         SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-//        factory.setConnectTimeout(2_000); // 연결 타임아웃 2초
-//        factory.setReadTimeout(2_000); // 읽기 타임아웃 2초
-
         factory.setConnectTimeout(300); // 연결 타임아웃 300ms
         factory.setReadTimeout(500); // 읽기 타임아웃 500ms
 
         return RestClient.builder()
                 .requestFactory(factory)
+                .defaultHeader("X-Internal-Token", sharedSecret)
                 .build();
     }
 }
